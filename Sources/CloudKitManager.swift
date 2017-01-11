7def957765db79cb31aadca8a88621b5deee5085
Andrew Madsen <andrew@openreelsoftware.com>
2017-01-11 15:04:59 -0700

    Fix use hardcoded Message.recordType in CloudKitManager subscribe() method.

--- Sources/CloudKitManager.swift
+++ Sources/CloudKitManager.swift
@@ -37,7 +37,7 @@ class CloudKitManager {
 	}
 	
 	func subscribeToCreationOfRecords(ofType type: String, completion: @escaping ((Error?) -> Void) = { _ in }) {
-		let subscription = CKQuerySubscription(recordType: Message.recordType, predicate: NSPredicate(value: true), options: .firesOnRecordCreation)
+		let subscription = CKQuerySubscription(recordType: type, predicate: NSPredicate(value: true), options: .firesOnRecordCreation)
 
 		let notificationInfo = CKNotificationInfo()
 		notificationInfo.alertBody = "There's a new message on the bulletin board."
